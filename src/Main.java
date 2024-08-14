bcefec8de0ba4753a883e1f6baa8dea24d246f87
rayen-elA <melojelo420@gmail.com>
2024-08-14 15:15:39 +0100

    [ADDED]added button functions and setted the frame

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,178 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import tree.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    static TreeNode tree = null;
+    static List<Integer> list= new ArrayList<>();
+    public static void main(String[] args) {
+
+TreeService service=new TreeService(tree);
+        JPanel sidepanel=new JPanel();
+        JTextArea sidepanelarea =new JTextArea();
+        sidepanel.add(sidepanelarea);
+
+                // Create the main frame
+                JFrame frame = new JFrame("Split Panel Example");
+                frame.setSize(1200, 400);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLayout(new BorderLayout());
+
+                // Create the top panel with GridLayout
+                JPanel topPanel = new JPanel(new GridLayout(1, 4));
+
+                // Create buttons and text fields
+                JButton button1 = new JButton("Add Node");
+                JTextField textField1 = new JTextField();
+                JButton button2 = new JButton("Delete Node");
+                JTextField textField2 = new JTextField();
+                JButton button3 = new JButton("DFS Search");
+                JTextField textField3 = new JTextField();
+                JButton button4 = new JButton("Exit");
+        textField1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                button1.doClick(); // Simulate a button click
+            }
+        });
+
+// Adding action listener to textField2 to trigger button2's action
+        textField2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                button2.doClick(); // Simulate a button click
+            }
+        });
+
+// Adding action listener to textField3 to trigger button3's action
+        textField3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                button3.doClick(); // Simulate a button click
+            }
+        });
+        Action escapeAction = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                button4.doClick(); // Simulate button4 click
+            }
+        };
+
+// Bind the Escape key to the escapeAction in the root pane of the frame
+        frame.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("ESCAPE"), "escapeAction");
+        frame.getRootPane().getActionMap().put("escapeAction", escapeAction);
+
+                // Add buttons and text fields to the top panel
+                topPanel.add(button1);
+                topPanel.add(textField1);
+                topPanel.add(button2);
+                topPanel.add(textField2);
+                topPanel.add(button3);
+                topPanel.add(textField3);
+                topPanel.add(button4);
+
+                // Create the bottom panel for drawing
+                JPanel bottomPanel = new JPanel();
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, topPanel, bottomPanel);
+        splitPane.setDividerSize(10); // Make the divider thicker
+        splitPane.setDividerLocation(150); // Initial position of the divider
+        splitPane.setOneTouchExpandable(true);
+        splitPane.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        JLabel displayLabel = new JLabel("Text will be displayed here");
+        displayLabel.setFont(new Font("Arial", Font.PLAIN, 16));
+        bottomPanel.add(displayLabel);
+
+        // Add action listeners to the buttons to display the text from the text fields
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                frame.revalidate();
+                frame.repaint();
+
+                    String valueholder=textField1.getText();
+                    try{
+                        int value=Integer.parseInt(valueholder);
+                        tree=service.addNode(tree,value);
+                        sidepanelarea.append("value "+value+ " added.\n");
+                    }
+                    catch(NumberFormatException asba){
+                        sidepanelarea.append("brother,give a number please.\n");
+                    }
+
+
+
+            }
+        });
+
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String valueholder=textField2.getText();
+                TreeNode parent=null;
+                try{
+                    int value=Integer.parseInt(valueholder);
+                    Thread.sleep(30);
+
+
+                    if(!(service.searchTree(tree,value,parent,sidepanelarea)instanceof Boolean) && tree!=null) {
+                        tree =service.deleteNode(tree,value,sidepanelarea);
+                        sidepanelarea.append("value " + value + " deleted.\n");
+                    }
+                }
+                catch(NumberFormatException | InterruptedException | NullPointerException asba ){
+                    if(tree==null)
+                        sidepanelarea.append("tree is empty.\n");
+                    else
+                    sidepanelarea.append("brother,give a number please.\n");
+                }
+            }
+        });
+
+        button3.addActionListener(new ActionListener() {
+            @Override
+
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    String valueholder=textField3.getText();
+                    int value=Integer.parseInt(valueholder);
+                    if(service.searchTreeDFS(tree,value,list))
+                    {
+                        sidepanelarea.append("path is " +list+" \n");
+
+                    }
+                }
+                catch(NumberFormatException asba){
+                    sidepanelarea.append("brother,give a number please.\n");
+                }
+
+
+            }
+        });
+        button4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0); // Close the frame
+            }
+        });
+
+
+                // Add the panels to the frame
+                frame.add(topPanel, BorderLayout.NORTH);
+                frame.add(bottomPanel, BorderLayout.CENTER);
+                frame.add(sidepanel,BorderLayout.EAST);
+
+                // Make the frame visible
+                frame.setVisible(true);
+            }
         }
-    }
-}
+
+
+
+
+
---
+++ src/tree/TreeService.java
@@ -0,0 +1,221 @@
+package tree;
+
+
+import javax.swing.*;
+import java.util.List;
+
+public class TreeService {
+    public TreeNode tree;
+    public boolean check=true;
+
+    public TreeService(TreeNode tree) {
+        this.tree = tree;
+
+    }
+
+    public TreeNode setTreeRoot(int val, TreeNode tree) {
+        TreeNode tree1=new TreeNode(val);
+
+        return tree1;
+    }
+
+    public void printTree(TreeNode tree) {
+
+        if (tree != null) {
+            System.out.print(tree.getVal() + " | ");
+            printTree(tree.getRight());
+            printTree(tree.getLeft());
+        }
+        else
+            return;
+
+
+    }
+
+    public TreeNode addNode(TreeNode tree, int val) {
+        TreeNode current = tree;
+        TreeNode newNode = new TreeNode(val);
+
+        TreeNode parent;
+        if (tree == null) {
+            tree = newNode;
+        } else {
+            while (true) {
+                parent = current;
+                if (val < current.getVal()) {
+                    current = current.getLeft();
+                    if (current == null) {
+                        parent.setLeft(newNode);
+                        break;
+                    }
+
+                } else {
+                    current = current.getRight();
+                    if (current == null) {
+                        parent.setRight(newNode);
+                        break;
+                    }
+
+                }
+            }
+
+        }
+        return  tree;
+
+
+        //printTree(tree);
+
+
+
+    }
+
+
+    public Object searchTree(TreeNode current,int val, TreeNode parent,JTextArea sidepanelarea){
+        while (true) {//looking for node to delete
+            if (current.getVal() == val) {
+                return parent;
+            }
+            parent = current;
+            if (val < current.getVal()) {
+                current = current.getLeft();
+                if (current == null) {
+                    check=false;
+                    sidepanelarea.append("sorry," + val + "doesnt exist\n");
+                    return false;
+                }
+            } else {
+                current = current.getRight();
+                if (current == null) {
+                    sidepanelarea.append("sorry" + val + "doesnt exist\n");
+                    check=false;
+                    return false;}
+            }
+        }
+    }
+    public TreeNode deleteNode(TreeNode tree, int val, JTextArea sidepanelarea) {
+        TreeNode current = tree;
+        TreeNode parent=null;
+
+
+
+        if (tree == null) {// tree is empty nothing to delete
+            check=false;
+            sidepanelarea.append("tree is empty \n");
+        } else {
+            if ( searchTree(tree, val,parent,sidepanelarea) instanceof Boolean){
+                return tree;
+            }else {
+                parent = (TreeNode) searchTree(tree, val, parent,sidepanelarea);
+                if (parent != null) {
+
+                    if (parent.getLeft() != null) {
+                        if (parent.getLeft().getVal() == val)
+                            current = parent.getLeft();
+                    }
+                    if(parent.getRight()!=null) {
+                        if (parent.getRight().getVal() == val)
+                            current = parent.getRight();
+                    }
+
+
+                }
+            }
+
+
+            if (current.getRight() == null && current.getLeft() == null) {//if deleting a leaf
+                if (parent == null) {
+                    return null;
+                } else if (parent.getLeft() == current) {
+                    parent.setLeft(null);
+                } else {
+                    parent.setRight(null);
+                }
+                return tree;
+            }
+            if (current.getRight() == null && current.getLeft() != null) {//if there's one child on the left
+                if (parent != null) {//node to delete isnt the root
+                    if (parent.getLeft() == current) {//if node to delete on the left of the parent
+                        parent.setLeft(current.getLeft());//skipping over the node to delete
+                    } else {
+                        parent.setRight(current.getLeft());//skipping over the node to delete
+                    }
+                } else {//node to delete is root so just skip over
+                    tree = tree.getLeft();
+                }
+                return tree;
+            } else if (current.getLeft() == null && current.getRight() != null) {//node to delete has a child on the right
+                if (parent != null) {
+                    if (parent.getLeft() == current) {
+                        parent.setLeft(current.getRight());
+                    } else {
+                        parent.setRight(current.getRight());
+                    }
+                } else {
+                    tree = tree.getRight();
+
+                }
+
+            }
+            if ((current.getLeft() != null) && (current.getRight() != null)) {//node to delete has 2 children
+                if (parent == null) {//node to delete is the root
+                    TreeNode x = searchSmallestNode(current.getRight());
+                    current.setVal(x.getVal());
+                    if(x.getLeft()!=null) {
+                        x.getLeft().setRight(x.getRight());
+                        current.setRight(x.getLeft());
+                    }
+                    else
+                        current.setRight(x.getRight());
+                    return tree;
+                } else {
+                    TreeNode treeNode = searchSmallestNode(current.getRight());
+                    if (treeNode == current.getRight()) {
+                        if (current.getRight().getLeft() != null) {
+                            current.setVal(current.getRight().getLeft().getVal());
+                            current.getRight().setLeft(null);
+                        } else {
+                            current.setVal(current.getRight().getVal());
+                            current.setRight(current.getRight().getRight());
+                        }
+                    } else {
+                        current.setVal(treeNode.getLeft().getVal());
+                        treeNode.setLeft(treeNode.getLeft().getRight());
+                    }
+                }
+                return tree;
+            }
+        }
+        return tree;
+    }
+
+    public boolean searchTreeDFS(TreeNode tree, int val, List<Integer> path) {
+        if (tree == null)
+            return false;
+        path.add(tree.getVal());
+        if (tree.getVal() == val)
+            return true;
+        if (searchTreeDFS(tree.getRight(), val, path))
+            return true;
+        if (searchTreeDFS(tree.getLeft(), val, path))
+            return true;
+        path.removeLast();
+        return false;
+
+    }
+
+    public TreeNode searchSmallestNode(TreeNode tree) {
+        TreeNode treeNode = tree;
+        while (treeNode.getLeft() != null) {
+
+            while ((treeNode.getLeft().getLeft()) != null) {
+
+                treeNode = treeNode.getLeft();
+            }
+            if(treeNode.getLeft() != null && ((treeNode.getLeft().getLeft()) == null) )
+                break;
+        }
+        return treeNode;
+    }
+
+}
+
